8a3dacacaf66a46ad4c1eecb5dea5e6376be8815
Selvaraj Perumal <rajspselva@gmail.com>
2018-05-30 01:37:15 +1000

    fixes

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,27 @@
-$(document).ready(function()
-{
-    $("#button").click(function()
-  {
-    //var second='<div><img src="images1.jpg" alt="Processing.."/></div>';
-    if ($("#email").val() == 'admin' && $("#password").val() == 'admin')
-     {
-       $("#tt").load('samples/register.html');
-       //$("#tt").replaceWith("<h1>welcome my registration page</h1> ");
-       //$(this).hide().after().parent().append(second);
-      }
-      else {
-      alert("please try again");
-      }
-
-
-  });
-
-
-
+$(document).ready(function() {
+    $("#btnLogin").click(function() {
+        if (validation()) {
+            if ($("#txtUserName").val() == "admin" &&
+                $("#txtPassword").val() == "admin") {
+                return true;
+            } else {
+                alert("Please try again...")
+                return false;
+            }
+        }
+    });
 });
+
+function validation() {
+    var name = $("#txtUserName").val();
+    var password = $("#txtPassword").val();
+    if (name == '' || password == '') {
+        alert('Enter username and password');
+        return false;
+    } else if (password.length < 4) {
+        alert('password must be atleast 4 characters ');
+        return false;
+    } else {
+        return true;
+    }
+}
